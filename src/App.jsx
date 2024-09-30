74e29fdf504865d36f69f6e62cceb4d6c9ba1ea0
hemanth-92 <131354764+hemanth-92@users.noreply.github.com>
2024-09-30 09:34:32 +0530

    clean up

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+
+
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric",
+    );
+    const jsonData = await data.json();
+    console.log(jsonData)
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="flex min-h-screen flex-col bg-gray-900 text-white">
+      <main className="flex flex-grow items-center justify-center m-4">
+        <div className="w-3/4 h-full">
+\        </div>
+      </main>
+    </div>
+  );
 }
-
-export default App
